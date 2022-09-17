8d812818907685cdbe48ac0a24ff9f1cea829b97
BeatrizCastro274 <111613357+BeatrizCastro274@users.noreply.github.com>
2022-09-16 21:51:41 -0300

    2 files modified

--- src/app/Componentes/beatriz-radom-numbers/beatriz-radom-numbers.component.ts
+++ src/app/Componentes/beatriz-radom-numbers/beatriz-radom-numbers.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-beatriz-radom-numbers',
   templateUrl: './beatriz-radom-numbers.component.html',
-  styleUrls: ['./beatriz-radom-numbers.component.css']
+  styleUrls: ['./beatriz-radom-numbers.component.css'],
 })
 export class BeatrizRadomNumbersComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  shuffledArray: [];
+  @Input() m: string;
+  shuffleArray(array) {
+    var m = array.length,
+      t,
+      i;
 
-  ngOnInit() {
+    while (m) {
+      i = Math.floor(Math.random() * m--);
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
   }
-
+  ngOnInit() {}
 }
